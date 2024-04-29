9dee490dc9247c94d2440668a5dce4d8df1224c9
Gildemberg Leite <gildembergleite@gmail.com>
2024-04-29 11:17:22 -0300

    feat: create answer question use case

---
+++ src/domain/use-cases/answer-question.ts
@@ -0,0 +1,19 @@
+import { Answer } from '../entities/answer'
+
+interface AnswerQuestionUseCaseRequest {
+  instructorId: string
+  questionId: string
+  content: string
+}
+
+export class AnswerQuestionUseCase {
+  execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+    const answer = new Answer(content)
+  }
+}
+
+new AnswerQuestionUseCase().execute({
+  instructorId: '1',
+  questionId: '2',
+  content: '3',
+})
